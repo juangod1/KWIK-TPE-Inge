a49f764eb3485ced77fcd3a8d5c3c95265d0ffc1
pabloito <pradnic@itba.edu.ar>
2017-12-07 11:27:32 -0300

    Parser advancements

--- src/Controller/InputController.java
+++ src/Controller/InputController.java
@@ -14,7 +14,7 @@ public class InputController {
             int code = fe.getCode();
             switch (code){
                 case 1:
-                    if(!checkUserDNI(fe.getString())) return false;
+                    if(!checkUserDocument(fe.getString())) return false;
                     break;
                 case 2:
                     if(!checkUserEmail(fe.getString())) return false;
@@ -29,7 +29,7 @@ public class InputController {
                     if(!checkUserBirthDate(fe.getString())) return false;
                     break;
                 case 6:
-                    if(!checkUserDNIType(fe.getString())) return false;
+                    if(!checkUserDocumentType(fe.getString())) return false;
                     break;
                 case 7:
                     if(!checkUserNeighbourhood(fe.getString())) return false;
@@ -61,47 +61,86 @@ public class InputController {
         }
         return true;
     }
-    public boolean checkUserDNI(String dni){
-
+    private boolean isPositiveInteger(String str){
+        for (char c : str.toCharArray())
+        {
+            if (!Character.isDigit(c)) return false;
+        }
+        return true;
+    }
+    public boolean checkUserDocument(String dni){
+        if(dni.length()!=8) return false;
+        return isPositiveInteger(dni);
     }
     public boolean checkUserEmail(String email){
+        String[] strs= email.split("@");
+        return (strs.length==2 && !strs[0].isEmpty() && !strs[1].isEmpty());
+    }
+    public boolean checkUserPassword(String password){
+        return !password.isEmpty();
 
     }
-    public boolean checkUserPassword(String email){
+    public boolean checkUserNeighbourhood(String neighbourhood){
+        return !neighbourhood.isEmpty();
+    }
+    public boolean checkUserAddress(String address){
+
+        String[] strs= address.split(" ");
+        for (String s: strs){
+            if(s.isEmpty()) return false;
+        }
+        return strs.length>1;
 
     }
-    public boolean checkUserNeighbourhood(String email){
+    public boolean checkUserPhoneNumber(String phoneNumber){
+        if(phoneNumber.length()!=8) return false;
+        for (char c : phoneNumber.toCharArray())
+        {
+            if (!Character.isDigit(c)) return false;
+        }
+        return true;
 
     }
-    public boolean checkUserAddress(String email){
+    public boolean checkUserState(String state){
+        return !state.isEmpty();
+    }
+    public boolean checkUserPostalCode(String postalCode){
+        return !postalCode.isEmpty();
 
     }
-    public boolean checkUserPhoneNumber(String email){
-
+    public boolean checkUserDocumentType(String documentType){
+        return documentType.equals("DNI");
     }
-    public boolean checkUserState(String email){
-
+    public boolean checkUserBirthDate(String birthDate){
+        String[] strs = birthDate.split("/");
+        if(strs.length!=3 || isPositiveInteger(strs[0]) || isPositiveInteger(strs[1]) || isPositiveInteger(strs[2])) return false;
+        int int1=Integer.parseInt(strs[0]);
+        int int2=Integer.parseInt(strs[1]);
+        int int3=Integer.parseInt(strs[2]);
+        return (int1<32 && int1>0 && int2<13 && int2>0 && int3>1900 && int3<2018);
     }
-    public boolean checkUserPostalCode(String email){
-
+    public boolean checkProductTitle(String title){
+        return !title.isEmpty();
     }
-    public boolean checkUserDNIType(String email){
-
+    public boolean checkProductPrice(String price){
+        double a;
+        try
+        {
+            a=Double.parseDouble(price);
+        }
+        catch(NumberFormatException e)
+        {
+            return false;
+        }
+        return a>=0;
     }
-    public boolean checkUserBirthDate(String email){
-
-    }
-    public boolean checkProductTitle(String email){
-
-    }
-    public boolean checkProductPrice(String email){
-
-    }
-    public boolean checkProductAmount(String email){
-
+    public boolean checkProductAmount(String amount){
+        if(isPositiveInteger(amount)) return false;
+        int i = Integer.parseInt(amount);
+        return i>0;
     }
     public boolean checkProductDescription(String email){
-
+        return true;
     }
 }
 
