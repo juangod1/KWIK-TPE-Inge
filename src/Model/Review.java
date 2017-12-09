d2c455067ab3af2a4b26143575807c197f2c17c3
lucas <lucas.emery@hotmail.com>
2017-12-09 17:05:04 -0300

    DB review

--- src/Model/Review.java
+++ src/Model/Review.java
@@ -1,5 +1,9 @@
 package Model;
 
+import Service.DatabaseService;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.ArrayList;
 
 public class Review implements Persistent{
@@ -7,6 +11,10 @@ public class Review implements Persistent{
     private Product product;
     private String comment;
     private int rating;
+
+    private int productId;
+
+    private static final String select = "SELECT * FROM review ";
 
     private Review() {}
 
@@ -17,16 +25,57 @@ public class Review implements Persistent{
         this.rating = rating;
     }
 
+    private Review(int id, int productId, String comment, int rating) {
+        this.id = id;
+        this.productId = productId;
+        this.product = null;
+        this.comment = comment;
+        this.rating = rating;
+    }
+
     public static Review create(Product product, String comment, int rating) {
-        return new Review(0, product, comment, rating);
+        try {
+            ResultSet rs = DatabaseService.getInstance().getSt().executeQuery("INSERT INTO review (idproduct, comment, rating) " +
+                    "VALUES ("+product.getId()+", "+comment+", "+rating+") RETURNING id");
+            if(rs.next())
+                return new Review(rs.getInt(1), product, comment, rating);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return null;
     }
 
     public static Review get(int id) {
-        return new Review();
+        try {
+            ResultSet rs = DatabaseService.getInstance().getSt().executeQuery(select + "WHERE id = "+id);
+            if(rs.next())
+                return fromResultSet(rs);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return null;
     }
 
     public static ArrayList<Review> list(Product product) {
-        return new ArrayList<>();
+        ArrayList<Review> reviews = new ArrayList<>();
+        try {
+            ResultSet rs = DatabaseService.getInstance().getSt().executeQuery(select + "WHERE idproduct = "+product.getId());
+            while (rs.next())
+                reviews.add(fromResultSet(rs));
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return reviews;
+    }
+
+    private static Review fromResultSet(ResultSet rs) {
+        try {
+            return new Review(rs.getInt("id"), rs.getInt("idProduct"), rs.getString("comment"),
+                    rs.getInt("rating"));
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return null;
     }
 
     @Override
@@ -44,6 +93,8 @@ public class Review implements Persistent{
     }
 
     public Product getProduct() {
+        if(product == null)
+            product = Product.get(productId);
         return product;
     }
 
