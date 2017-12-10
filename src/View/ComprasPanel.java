9c50e3f5717d965da42bea59469d466a3db9dc16
lucas <lucas.emery@hotmail.com>
2017-12-10 20:02:35 -0300

    Revert "Hice que compile, todo lo de este comit se puede volar al carajo"
    This reverts commit 0776294985a3b4c6fcb496c4ca95c4905221d862.

--- src/View/ComprasPanel.java
+++ src/View/ComprasPanel.java
@@ -40,13 +40,14 @@ public class ComprasPanel {
     private static final int PAGESIZE = 5;
     private ArrayList<Product> products;
     private HashMap<Product,Integer> ids;
-    private Controller controller;
 
 
 
     public ComprasPanel(final View.ViewSwapper vs, final Controller controller) {
         this.vs = vs;
-        this.controller = controller;
+        this.offset = 0;
+        this.ids = Product.listBoughtProducts(controller.getCurrentUser());
+        this.products = new ArrayList<>(ids.keySet());
         anteriorButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
@@ -144,10 +145,8 @@ public class ComprasPanel {
     }
     public void printResults(final ArrayList<Product> prods){
         cleanPanel();
-        offset = 0;
-        ids = Product.listBoughtProducts(controller.getCurrentUser());
-        products = new ArrayList<>(ids.keySet());
-        if(products.size()>PAGESIZE){
+        offset=0;
+        if(prods.size()>PAGESIZE){
             siguienteButton.setEnabled(true);
         }
         refresh();
@@ -155,30 +154,27 @@ public class ComprasPanel {
     private void refresh(){
         ArrayList<Product> productList = new ArrayList<>();
         cleanPanel();
-        int size = products.size();
+        int size = prods.size();
         if(size >= 1 + offset) {
-            Product prod = products.get(offset);
+            Product prod = prods.get(offset);
 
         }
         if(size>=2+offset) {
-            Product prod = products.get(offset+1);
+            Product prod = prods.get(offset+1);
         }
         if(size>=3+offset) {
-            Product prod = products.get(offset+2);
+            Product prod = prods.get(offset+2);
 
         }
         if(size>=4+offset) {
-            Product prod = products.get(offset+3);
+            Product prod = prods.get(offset+3);
 
         }
         if(size>=5+offset) {
-            Product prod = products.get(offset+4);
+            Product prod = prods.get(offset+4);
 
         }
     }
-
-    private void cleanPanel(){}
-
     public JPanel getMainpanel() {
         return mainpanel;
     }
